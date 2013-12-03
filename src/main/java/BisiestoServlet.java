a52978d9af997ccce69eed113c8dbb8c72e27aa6
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2013-12-03 08:20:19 -0400

    Respuesta5

--- src/main/java/BisiestoServlet.java
+++ src/main/java/BisiestoServlet.java
@@ -5,14 +5,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class BisiestoServlet extends HttpServlet {
-	
+	Anio anio = new Anio();
+
 	@Override
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String anio = request.getParameter("anio");
-		response.getWriter().println(anio);
+		String year = request.getParameter("anio");
+		anio.setAnio(Integer.parseInt(year));
+		if (anio.esBisiesto()) {
+			response.getWriter().println(
+					"El anio " + anio.getAnio() + " es Bisiesto!");
+		} else {
+			response.getWriter().println(
+					"El anio " + anio.getAnio() + " No es Bisiesto!");
+		}
 	}
-
 }
